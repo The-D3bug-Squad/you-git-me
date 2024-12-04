0c2c120d21a5af003ed4a5c811a142db5f07ce2b
Thato Mabuela <mabuelathato03@gmail.com>
2024-12-04 19:44:38 +0200

    Update helper_functions.py , main.py and test_functions.py

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -1,5 +1,6 @@
 import pwinput
 from termcolor import colored
+import csv
 
 # try and accept accordingly to make a program that will not crash
 # learn what the type hints are and how to use them e.g. -> str, -> bool, -> int
@@ -9,10 +10,21 @@ def get_password(prompt: str) -> str:
     Get a password from the user.
     """
 
+    try:
+        password = pwinput.pwinput(prompt=prompt)
+        return password
+    except:
+        return 
+
 def get_username(prompt: str) -> str:
     """
     Get a username from the user.
     """
+    try:
+        username = input(prompt)
+        return username
+    except:
+        return
 
 def validate_password(password: str) -> bool:
     """
@@ -24,6 +36,29 @@ def validate_password(password: str) -> bool:
     - at least one digit
     - at least one special character
     """
+    if len(password) >= 8:
+        upper = 0
+        lower = 0
+        digit = 0
+        special = 0
+        for char in password:
+            if char.isupper():
+                upper += 1
+            elif char.islower():
+                lower += 1
+            elif char.isdigit():
+                digit += 1
+            else:
+                if char.isalnum():
+                    continue
+                else:
+                    special += 1
+        if upper > 0 and lower > 0 and digit > 0 and special > 0:
+            return True
+        else:
+            return False
+    else:
+        return False 
 
 def validate_username(username: str) -> bool:
     """
@@ -32,6 +67,15 @@ def validate_username(username: str) -> bool:
     - at least 3 characters
     - no alphanumeric characters
     """
+    if len(username) >= 3:
+        for char in username:
+            if char.isalnum():
+                continue
+            else:
+                return False
+        return True
+    else:
+        return False
 def save_user_info(username: str, password: str) -> None:
     database_path = "./Database/users.csv"
     """
@@ -42,7 +86,18 @@ def save_user_info(username: str, password: str) -> None:
     - append the username and password to the file
     - ensure theres no duplicates
     """
-
+    try:
+        with open(database_path,"a") as file:
+            with open(database_path,"r") as f:
+                reader = csv.reader(f)
+                for line in reader:
+                    if [username,password] == line:
+                        return "Username and password already exists"
+                info = f"{username},{password}"
+                file.write(f"{str(info)}\n")
+                return "You have been successfully added to the database"
+    except:
+        return "Failed to add to database"
 
 if __name__ == "__main__":
     #use to test the functions
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-# import the tools
-
+from Tools.helper_functions import get_password, get_username, validate_password, validate_username, save_user_info
+import time
 
 def main():
     #implement logic for the program to work correctly use given functions you implemented
-    pass
+    print("Hello, please enter your credentials.")
+    username = get_username("Username: ")
+    password = get_password("Password: ")
+    print("Checking password validity...")
+    time.sleep(1)
+    if validate_password(password):
+        print("Checking username validity...")
+        time.sleep(1)
+        if validate_username(username):
+            print("Adding you to database...")
+            time.sleep(1)
+            print(save_user_info(username,password))
+    else:
+        print("Invalid Password")
+    print()
 
 
 
