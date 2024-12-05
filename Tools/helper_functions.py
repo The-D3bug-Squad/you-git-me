75e15053f6339838898fb2f9d9cae0b4478f91d9
simhlonjhb024 <simhlonjhb024@student.wethinkcode.co.za>
2024-12-05 11:44:53 +0200

    Just finished

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -1,5 +1,5 @@
 import pwinput
-from termcolor import colored
+# from termcolor import colored
 
 # try and accept accordingly to make a program that will not crash
 # learn what the type hints are and how to use them e.g. -> str, -> bool, -> int
@@ -8,11 +8,18 @@ def get_password(prompt: str) -> str:
     """
     Get a password from the user.
     """
+    password = pwinput.pwinput(prompt, mask="🙈")
+
+
+    return password
 
 def get_username(prompt: str) -> str:
     """
     Get a username from the user.
     """
+    username = input(prompt)
+
+    return username
 
 def validate_password(password: str) -> bool:
     """
@@ -24,6 +31,25 @@ def validate_password(password: str) -> bool:
     - at least one digit
     - at least one special character
     """
+    hasUpper = False
+    hasLower = False
+    hasDigit = False
+    hasSpecial = False
+
+    if len(password) < 8:
+        return False
+    for char in password:
+        if char.isupper():
+            hasUpper = True
+        if char.islower():
+            hasLower = True
+        if char.isdigit():
+            hasDigit = True
+        if not any([char.isupper(), char.islower(), char.isdigit()]):
+            hasSpecial = True
+
+    return hasUpper and hasLower and hasDigit and hasSpecial
+
 
 def validate_username(username: str) -> bool:
     """
@@ -32,6 +58,18 @@ def validate_username(username: str) -> bool:
     - at least 3 characters
     - no alphanumeric characters
     """
+   
+    if len(username) < 3:
+        return False
+    
+    
+    for char in username:
+        if char.isalnum() == False: 
+            return False
+    
+    
+    return True
+
 def save_user_info(username: str, password: str) -> None:
     database_path = "./Database/users.csv"
     """
@@ -42,7 +80,8 @@ def save_user_info(username: str, password: str) -> None:
     - append the username and password to the file
     - ensure theres no duplicates
     """
-
+    with open(database_path, "a") as file:
+        file.write(f"{username},{password}\n")
 
 if __name__ == "__main__":
     #use to test the functions
