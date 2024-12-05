6e8ca6cd2c1c37a3d939cc4e0710c8383936a33d
chrysan22 <120726246+chrysan22@users.noreply.github.com>
2024-12-05 23:06:20 +0200

    Update helper_functions.py

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -10,7 +10,7 @@ def get_password(prompt: str) -> str:
     """
     Get a password from the user.
     """
-   return pwinput.pwinput(prompt)
+    return pwinput.pwinput(prompt)
 
 password = get_password("Enter your password: ")
     
