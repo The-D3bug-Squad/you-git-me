804064b0cd90a914ab9792d49f1fda1813392a32
Osmond2 <osmatsejhb024@student.wethinkcode.co.za>
2024-12-05 13:03:40 +0200

    final commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # import the tools
+from Tools.helper_functions import *
 
 
 def main():
     #implement logic for the program to work correctly use given functions you implemented
-    pass
+    username  = get_username('Enter your user name: ')
+    password = get_password('Enter your password: ')
+    if validate_username(username) and validate_password(password):
+        save_user_info(username,password)
+    else:
+        return "Invalid password"
+
 
 
 
 if __name__ == "__main__":
     main()
+
