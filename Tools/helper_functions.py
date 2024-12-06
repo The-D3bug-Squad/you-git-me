99aef01cda3602731077da6dd63cbfbb92a821bc
Glockrover <ayandadlokovich@icloud.com>
2024-12-06 15:24:14 +0200

    done with some of the tests

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -1,5 +1,6 @@
 import pwinput
 from termcolor import colored
+import csv
 
 # try and accept accordingly to make a program that will not crash
 # learn what the type hints are and how to use them e.g. -> str, -> bool, -> int
@@ -8,11 +9,19 @@ def get_password(prompt: str) -> str:
     """
     Get a password from the user.
     """
+    try:
+        return pwinput.pwinput(prompt=prompt)
+    except ValueError:
+        pass
 
 def get_username(prompt: str) -> str:
     """
     Get a username from the user.
     """
+    try:
+        return input(prompt)
+    except ValueError:
+        pass
 
 def validate_password(password: str) -> bool:
     """
@@ -24,6 +33,27 @@ def validate_password(password: str) -> bool:
     - at least one digit
     - at least one special character
     """
+    length = False
+    number = False
+    upper_case = False
+    lower_case = False
+    special = False
+
+    if len(password) >= 8:
+        length = True
+        for letter in password:
+                if letter.isdigit():
+                    number = True
+                if letter.isupper():
+                    upper_case = True
+                if letter.islower():
+                    lower_case = True 
+                if letter in "!, @, #, $, %, ^, &, *, (, ), _, +, =, {, },, |, ;, :, \", ', <, >, /, ?":
+                    special = True
+    else:
+        return False
+    return length and number and lower_case and upper_case and special
+    
 
 def validate_username(username: str) -> bool:
     """
@@ -32,6 +62,8 @@ def validate_username(username: str) -> bool:
     - at least 3 characters
     - no alphanumeric characters
     """
+    return len(username) >= 3 and username.isalpha()
+
 def save_user_info(username: str, password: str) -> None:
     database_path = "./Database/users.csv"
     """
@@ -43,6 +75,19 @@ def save_user_info(username: str, password: str) -> None:
     - ensure theres no duplicates
     """
 
+    with open(database_path, "r") as database:
+        reader = csv.reader(database)
+        existing_usernames = [row[0] for row in reader]
+
+    if username in existing_usernames:
+        return f"{username} already exist"
+    else:
+        with open(database_path, 'a') as data:
+            writer = csv.writer(data)
+            writer.writerow([username, password])
+            return f"{username} added to database"
+
+print(save_user_info("Tshepiso", "id8fu89f"))
 
 if __name__ == "__main__":
     #use to test the functions
