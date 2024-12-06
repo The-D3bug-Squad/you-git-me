17338dba49efcf70ea7429df8f66dda011679abb
nhlakanipho <nqmasilela777@gmail.com>
2024-12-06 22:05:25 +0200

    "Functions work but test not passing unless I move helper function to tests folder

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -1,5 +1,7 @@
 import pwinput
 from termcolor import colored
+import string
+import csv
 
 # try and accept accordingly to make a program that will not crash
 # learn what the type hints are and how to use them e.g. -> str, -> bool, -> int
@@ -8,11 +10,13 @@ def get_password(prompt: str) -> str:
     """
     Get a password from the user.
     """
+    return input(prompt)
 
 def get_username(prompt: str) -> str:
     """
     Get a username from the user.
     """
+    return input(prompt)
 
 def validate_password(password: str) -> bool:
     """
@@ -25,6 +29,26 @@ def validate_password(password: str) -> bool:
     - at least one special character
     """
 
+    validity_code = '0 0 0 0'.split()
+    if len(password) < 8:
+        return False
+
+    for c in password:
+        if c in string.ascii_lowercase:
+            validity_code[0] = '1'
+        elif c in string.ascii_uppercase:
+            validity_code[1] = '1'
+        elif c.isdigit():
+            validity_code[2] = '1'
+        elif c in string.punctuation:
+            validity_code[3] = '1'
+
+    if ''.join(validity_code) == '1111':
+        return True
+    
+    return False
+        
+
 def validate_username(username: str) -> bool:
     """
     Validate a username.
@@ -32,6 +56,16 @@ def validate_username(username: str) -> bool:
     - at least 3 characters
     - no alphanumeric characters
     """
+
+    if len(username) < 3:
+        return False
+    
+    for c in username:
+        if c in string.punctuation:
+            return False
+        
+    return True
+
 def save_user_info(username: str, password: str) -> None:
     database_path = "./Database/users.csv"
     """
@@ -42,6 +76,22 @@ def save_user_info(username: str, password: str) -> None:
     - append the username and password to the file
     - ensure theres no duplicates
     """
+    try:
+        with open(database_path, 'w+') as db:
+            reader = csv.DictReader(db)
+            data = list(reader)
+            
+            new_user = {'username': username, 'password': password}
+
+            if new_user not in data:
+                data.append(new_user)
+
+            writer = csv.DictWriter(db, fieldnames=['username', 'password'])
+            writer.writeheader()
+            writer.writerows(data)
+                
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File does not exist check file path: {database_path}")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 # import the tools
+from Tools.helper_functions import *
 
 
 def main():
     #implement logic for the program to work correctly use given functions you implemented
-    pass
+    print("User authantication utility\n\n")
+    input(f"Press enter to create a user.\n\n")
 
+    username = ""
+    password = ""
 
+    while not validate_username(username):
+        username = get_username("Enter username: ")
+        print()
+        if not validate_username(username):
+            print("Username must have atleast 3 alphanumeric characters.")
+    
+    while not validate_password(password):
+        password = get_password("Enter password: ")
+        print()
+        if not validate_password(password):
+            print(""" a valid password should contain:
+    - at least 8 characters
+    - at least one uppercase letter
+    - at least one lowercase letter
+    - at least one digit
+    - at least one special character\n""")
+        
+    if validate_password(password) and validate_username(username):
+        save_user_info(username,password)
+        print("User info sucessfully saved.\n")
 
 if __name__ == "__main__":
     main()
