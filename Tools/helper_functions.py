5d5f6322e136688a66547ba88cafa72b74493d0c
namotswjhb024 <namotswjhb024@student.wethinkcode.co.za>
2024-12-06 12:51:58 +0200

    update

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -1,5 +1,7 @@
 import pwinput
+import re
 from termcolor import colored
+import csv
 
 # try and accept accordingly to make a program that will not crash
 # learn what the type hints are and how to use them e.g. -> str, -> bool, -> int
@@ -8,11 +10,15 @@ def get_password(prompt: str) -> str:
     """
     Get a password from the user.
     """
+    prompt = input("Enter your password: ")
+    return prompt
 
 def get_username(prompt: str) -> str:
     """
     Get a username from the user.
     """
+    prompt = input("Enter your username: ")
+    return prompt
 
 def validate_password(password: str) -> bool:
     """
@@ -24,7 +30,11 @@ def validate_password(password: str) -> bool:
     - at least one digit
     - at least one special character
     """
-
+    if len(password) >= 8 and re.search(r'[A-Z]',password) and re.search(r'[a-z]', password) and re.search(r'[0-9]', password) and re.search(r'[@!~`-]',password) and password.replace(" ", ""):
+        return True
+    
+    else:
+        return False
 def validate_username(username: str) -> bool:
     """
     Validate a username.
@@ -32,6 +42,11 @@ def validate_username(username: str) -> bool:
     - at least 3 characters
     - no alphanumeric characters
     """
+    if len(username) >= 3 and username != re.search(r'[@!~`-]', username):
+        return True
+    else:
+        return False
+    
 def save_user_info(username: str, password: str) -> None:
     database_path = "./Database/users.csv"
     """
@@ -42,6 +57,8 @@ def save_user_info(username: str, password: str) -> None:
     - append the username and password to the file
     - ensure theres no duplicates
     """
+with open('users.csv', mode = 'r') as file:
+    csvFile = csv.reader(file)
 
 
 if __name__ == "__main__":
