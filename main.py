42dc4aefcb630601f4eecc19f4b40910429387f4
mamdukajhb024 <mamdukajhb024@student.wethinkcode.co.za>
2024-12-06 08:30:49 +0200

    Add helper functions import to main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # import the tools
+from Tools.helper_functions import (
+    get_password,
+    get_username,
+    validate_password,
+    validate_username,
+    save_user_info,
+)
 
 
 def main():
-    #implement logic for the program to work correctly use given functions you implemented
+    # implement logic for the program to work correctly use given functions you implemented
     pass
-
 
 
 if __name__ == "__main__":
