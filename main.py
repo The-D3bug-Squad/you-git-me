ef78f074f1117f375976306c5bc04ea6919c70cc
chrysan22 <120726246+chrysan22@users.noreply.github.com>
2024-12-06 15:14:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # import the tools
+from Tools.helper_function.py import *
 
 
 def main():
     #implement logic for the program to work correctly use given functions you implemented
-    pass
+    username = get_username("Enter your username: ")
+    password = get_password("Enter your password: ")
+    
+    if validate_username(username) and validate_password(password):
+        save_user_info(username, password)
+    else:
+        return ValueError("Invalid username and password!")
 
 
 
