f300ad3cbb5a69537f6d440cc5cd28e5199574d5
Qiniso Xulu <qiniso.nhlakanipho.xulu@gmail.com>
2024-12-06 14:59:34 +0200

    Updated validate validate_password for checing when the user already exists in the database

--- Tools/user_functions.py
+++ Tools/user_functions.py
@@ -1,10 +1,12 @@
 import pwinput
+import csv
 
 def get_password(prompt: str) -> str:
     password = pwinput.pwinput(prompt); return password
 
 def get_username(prompt: str) -> str:
-    username = input(prompt); return username
+    username = input(prompt)
+    return username
 
 def validate_password(password: str) -> bool:
     if len(password) < 8: return False
@@ -12,6 +14,12 @@ def validate_password(password: str) -> bool:
     return has_upper and has_lower and has_digit and has_special
 
 def validate_username(username: str) -> bool:
+    database_path = "./Database/users.csv"
+    with open(database_path, "r") as file:
+        database = csv.DictReader(file)
+        for line in database:
+            for elements in line: 
+                if username in elements: return False
     return len(username) >= 3 and username.isalnum()
 
 def save_user_info(username: str, password: str) -> None:
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from Tools.user_functions import *
 
 def main():
-    password_input = get_password("Enter your password: ")
-    username_input = get_username("Enter your username: ")
-    if validate_password(password_input) and validate_username(username_input): save_user_info(username_input, password_input)
+    password_input = get_password("Enter your password: ");
+    username_input = get_username("Enter your username: ");
+    if validate_password(password_input) and validate_username(username_input): save_user_info(username_input, password_input);
     else:
-        if not validate_password(password_input): print("Invalid password. Please try again.")
-        if not validate_username(username_input): print("Invalid username. Please try again.")
+        if not validate_password(password_input): print("Invalid password. Please try again.");
+        if not validate_username(username_input): print("Either username already exists or username is invalid. Please try again.");
         
 if __name__ == "__main__":
     main()
