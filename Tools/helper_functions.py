2dd6ff46c001c5ce032b2ca5cdd081cd7343dfc1
mamabaljhb024 <mamabaljhb024@student.wethinkcode.co.za>
2024-12-10 16:46:41 +0200

    modified the files

--- Tools/helper_functions.py
+++ Tools/helper_functions.py
@@ -1,5 +1,6 @@
 import pwinput
-from termcolor import colored
+import termcolor
+from csv import writer
 
 # try and accept accordingly to make a program that will not crash
 # learn what the type hints are and how to use them e.g. -> str, -> bool, -> int
@@ -8,11 +9,15 @@ def get_password(prompt: str) -> str:
     """
     Get a password from the user.
     """
+    password = pwinput.pwinput(prompt=prompt)
+    return password
 
 def get_username(prompt: str) -> str:
     """
     Get a username from the user.
     """
+    prompt = input("enter your username: ")
+    return prompt
 
 def validate_password(password: str) -> bool:
     """
@@ -25,6 +30,34 @@ def validate_password(password: str) -> bool:
     - at least one special character
     """
 
+    if (len(password) < 8) :
+        return False
+    
+    has_digit = False
+    has_upper = False
+    has_lower = False
+    has_symbol = False
+
+
+    for character in password:
+        if character.islower():
+            has_lower = True
+        if character.isupper():
+            has_upper = True
+        if character.isdigit():
+            has_digit = True
+        if character in "!@#$%&*_-":
+            has_symbol = True
+
+    return has_symbol and has_lower and has_digit and has_upper
+    #return has_digit
+    # return has_lower
+        # print(has_symbol)
+        # print(has_upper)
+print(validate_password("wtdtd125@D"))
+
+
+
 def validate_username(username: str) -> bool:
     """
     Validate a username.
@@ -32,6 +65,22 @@ def validate_username(username: str) -> bool:
     - at least 3 characters
     - no alphanumeric characters
     """
+
+    if (len(username) < 3):
+        return False
+    has_digit = True
+    has_symbol = True
+
+    for character in username:
+        if character.isdigit():
+            has_digit = False
+        if character in  "!@#$%&*_-":
+            has_symbol = False
+
+    return has_symbol and has_digit
+
+
+
 def save_user_info(username: str, password: str) -> None:
     database_path = "./Database/users.csv"
     """
@@ -42,6 +91,19 @@ def save_user_info(username: str, password: str) -> None:
     - append the username and password to the file
     - ensure theres no duplicates
     """
+    with open(database_path,"") as f_objects:
+        f_objects.write(list) 
+
+        list = [username,password]
+
+        writer_object = writer(f_objects)
+
+        writer_object.writerow(list)
+
+        f_objects.close()
+
+    
+
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # import the tools
+from Tools import helper_functions
 
 
 def main():
     #implement logic for the program to work correctly use given functions you implemented
-    pass
+    count = 0
+    user_password = helper_functions.get_password(input("enter your password: "))
+    user_username = helper_functions.get_username(input("enter your username: "))
+    while helper_functions.validate_password(user_password)  == False:
+        user_password = helper_functions.get_password()
+    else:
+        count += 1
+    while helper_functions.validate_username(user_username) == False:
+        user_username = helper_functions.get_username()
+    else:
+        count += 1
 
+    if count == 2:
+        helper_functions.save_user_info(user_username,user_password)
 
+    
 
 if __name__ == "__main__":
     main()
